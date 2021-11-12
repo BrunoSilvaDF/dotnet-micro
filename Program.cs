10e5b74edce559473374bec5495fc0694c120832
Bruno Silva <brunoosilva@outlook.com>
2021-11-12 10:19:32 -0300

    feat: add prod terminal json loggin

--- Program.cs
+++ Program.cs
@@ -9,18 +9,30 @@ using Microsoft.Extensions.Logging;
 
 namespace DotnetMicro
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+
+            // Loggin in json
+            .ConfigureLogging((context, loggin) =>
+            {
+              if (context.HostingEnvironment.IsProduction())
+              {
+                loggin.ClearProviders();
+                loggin.AddJsonConsole();
+              }
+            })
+
+            // Api
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
